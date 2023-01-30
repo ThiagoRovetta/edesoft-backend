0bc69567eee02f46c982511985e6b25c71cc5598
ThiagoRovetta <rovetta.thiago@gmail.com>
2023-01-29 21:10:02 -0300

    Fixed environment variable.

--- app/db_access.py
+++ app/db_access.py
@@ -4,14 +4,14 @@ from psycopg2.extras import DictCursor
 
 
 AWS_DB_USER = os.environ['AWS_DB_USER']
-AWS_DB_PASSOWRD = os.environ['AWS_DB_PASSOWRD']
+AWS_DB_PASSWORD = os.environ['AWS_DB_PASSWORD']
 AWS_DB_HOST = os.environ['AWS_DB_HOST']
 AWS_DB_PORT = os.environ['AWS_DB_PORT']
 AWS_DB_NAME = os.environ['AWS_DB_NAME']
 
 
 def get_db_connection(cf=None):
-    conn = psycopg2.connect(user=AWS_DB_USER, password=AWS_DB_PASSOWRD,
+    conn = psycopg2.connect(user=AWS_DB_USER, password=AWS_DB_PASSWORD,
                             host=AWS_DB_HOST, port=AWS_DB_PORT,
                             database=AWS_DB_NAME)
 
